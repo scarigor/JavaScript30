16b0bc7286af7840636c364ad1645ab5c4e0352c
Igor Kurchenko <kurchenkoigor1@gmail.com>
2017-02-01 18:43:42 +0200

    finished clock

---
+++ 2. JS and CSS clock/scripts/script.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector('.sec-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+function setDate() {
+	const now = new Date();
+
+	const sec = now.getSeconds();
+	const secDegrees = ((sec / 60) * 360) + 90;
+	secondHand.style.transform = `rotate(${secDegrees}deg)`;
+
+	const min = now.getMinutes();
+	const minDegrees = ((min / 60) * 360) + 90;
+	minHand.style.transform = `rotate(${minDegrees}deg)`;
+
+	const hour = now.getMinutes();
+	const hourDegrees = ((hour / 12) * 360) + 90;
+	hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+
+setInterval(setDate, 1000);
+
+setDate();
