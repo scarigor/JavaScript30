7d1b5b1ff0f1586da54db81ee1c52ec31fc1ba09
Igor Kurchenko <kurchenkoigor1@gmail.com>
2017-02-02 15:21:59 +0200

    finished practice with CSS vars

--- 3. CSS Variables/scripts/script.js
+++ 3. CSS Variables/scripts/script.js
@@ -1,23 +1,10 @@
-const secondHand = document.querySelector('.sec-hand');
-const minHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
-function setDate() {
-	const now = new Date();
+const inputs = document.querySelectorAll('.controls input');
 
-	const sec = now.getSeconds();
-	const secDegrees = ((sec / 60) * 360) + 90;
-	secondHand.style.transform = `rotate(${secDegrees}deg)`;
-
-	const min = now.getMinutes();
-	const minDegrees = ((min / 60) * 360) + 90;
-	minHand.style.transform = `rotate(${minDegrees}deg)`;
-
-	const hour = now.getMinutes();
-	const hourDegrees = ((hour / 12) * 360) + 90;
-	hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+function handleUpdate() {
+	const suff = this.dataset.sizing || '';
+	document.documentElement.style.setProperty(`--${this.name}`, this.value + suff);
+	console.log(suff);
 }
 
-
-setInterval(setDate, 1000);
-
-setDate();
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
